620e1154c77a724700f3cffc1dad9aff15ae9fbc
Gustav K <g-u-r-r-a@hotmail.com>
2016-08-17 13:47:03 +0200

    Initial commit

---
+++ src/windbot_email/ListenThread.java
@@ -0,0 +1,145 @@
+package windbot_email;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
+
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.WatchEvent.Kind;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class ListenThread extends Thread
+{
+	Path listenpath;
+	String recipientEmail;
+	String senderEmail;
+	String senderPassword;
+	public ListenThread(Path listenpath, String recipientEmail, String senderEmail, char[] senderPassword)
+	{
+		this.listenpath = listenpath;
+		this.recipientEmail = recipientEmail;
+		this.senderEmail = senderEmail + "@gmail.com";
+		this.senderPassword = new String(senderPassword);
+		System.out.println(this.senderEmail + " and " + this.senderPassword);
+	}
+
+	@Override
+	public void run()
+	{
+		FileSystem fs = listenpath.getFileSystem();
+		try {
+			try(WatchService service = fs.newWatchService()){
+				listenpath.register(service, ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE); 
+	         // Start the infinite polling loop
+	            WatchKey key = null;
+	            while (true) {
+	                key = service.take();
+
+	                // Dequeueing events
+	                Kind<?> kind = null;
+	                for (WatchEvent<?> watchEvent : key.pollEvents()) {
+	                    // Get the type of the event
+	                    kind = watchEvent.kind();
+	                    if (OVERFLOW == kind) {
+	                        continue; // loop
+	                    } else if (ENTRY_CREATE == kind) {
+	                        // A new Path was created
+	                        Path newPath = ((WatchEvent<Path>) watchEvent)
+	                                .context();
+	                        // Output
+	                        System.out.println("New path created: " + newPath);
+	                        
+	                        Properties props = System.getProperties();
+	                        props.put("mail.smtp.starttls.enable", true);
+	                        props.put("mail.smtp.host", "smtp.gmail.com");
+	                        props.put("mail.smtp.user", senderEmail);
+	                        props.put("mail.smtp.password", senderPassword);
+	                        props.put("mail.smtp.port", "587");
+	                        props.put("mail.smtp.auth", true);
+
+
+
+	                        Session session = Session.getInstance(props,null);
+	                        MimeMessage message = new MimeMessage(session);
+
+	                        System.out.println("Port: "+session.getProperty("mail.smtp.port"));
+
+	                        try {
+	                            InternetAddress from = new InternetAddress(senderEmail);
+	                            message.setSubject("Windbot alert");
+	                            message.setFrom(from);
+	                            message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+
+	                            Multipart multipart = new MimeMultipart("alternative");
+
+	                            BodyPart messageBodyPart = new MimeBodyPart();
+	                            messageBodyPart.setText("Sent by Dolmero.");
+
+	                            multipart.addBodyPart(messageBodyPart);
+
+
+	                            messageBodyPart = new MimeBodyPart();
+
+	                           	String filename = listenpath + "\\" + newPath;
+	                           	System.out.println("2: " + filename);
+	                            DataSource source = new FileDataSource(filename);
+	                            messageBodyPart.setDataHandler(new DataHandler(source));
+	                            messageBodyPart.setFileName(filename);
+	                            multipart.addBodyPart(messageBodyPart);
+
+	                            message.setContent(multipart);
+
+	                            // Send message
+	                            Transport transport = session.getTransport("smtp");
+	                            transport.connect("smtp.gmail.com", senderEmail, senderPassword);
+	                            System.out.println("Transport: "+transport.toString());
+	                            transport.sendMessage(message, message.getAllRecipients());
+	                        } catch (AddressException e3) {
+	                            e3.printStackTrace();
+	                        } catch (MessagingException e2) {
+	                            e2.printStackTrace();
+	                        }
+	                        
+	                    } else if (ENTRY_MODIFY == kind) {
+	                        // modified
+	                        Path newPath = ((WatchEvent<Path>) watchEvent)
+	                                .context();
+	                        // Output
+	                        System.out.println("New path modified: " + newPath);
+	                    }
+	                }
+
+	                if (!key.reset()) {
+	                    break; // loop
+	                }
+	            }
+
+	        }  catch (InterruptedException ie) {
+	            ie.printStackTrace();
+	        }
+			
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+}
---
+++ src/windbot_email/Main.java
@@ -0,0 +1,85 @@
+package windbot_email;
+
+import java.nio.file.Path;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+
+import javax.swing.*;
+
+public class Main {
+	static String filepath;
+	private static void window(){
+		JFrame frame = new JFrame("Windbot Emailer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel clientInfo = new JPanel(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+		JLabel sendtoText = new JLabel("Email to");
+	    JTextField sendto = new JTextField(24);
+	    sendto.setText("windbotman@gmail.com");
+	    clientInfo.add(sendtoText, constraints);
+	    constraints.gridx = 1;
+	    clientInfo.add(sendto, constraints);
+	    
+	    constraints.gridx = 0;
+        constraints.gridy = 1;
+        JLabel sendfromText = new JLabel("Email from");
+        JLabel sendfromGmail = new JLabel("@gmail.com");
+	    JTextField sendfrom = new JTextField(17);
+	    sendfrom.setText("windbotman");
+	    clientInfo.add(sendfromText, constraints);
+	    constraints.gridx = 1;
+	    clientInfo.add(sendfrom, constraints);
+	    constraints.anchor = GridBagConstraints.EAST;
+	    clientInfo.add(sendfromGmail, constraints);
+	    constraints.anchor = GridBagConstraints.WEST;
+	    
+	    constraints.gridx = 0;
+        constraints.gridy = 2;
+        JLabel passwordText = new JLabel("Password");
+	    JPasswordField password = new JPasswordField(24);
+	    sendto.setText("");
+	    clientInfo.add(passwordText, constraints);
+	    constraints.gridx = 1;
+	    clientInfo.add(password, constraints);
+	      
+	    frame.getContentPane().add(clientInfo, BorderLayout.CENTER);
+	    JButton send = new JButton();
+	    send.setText("Send");
+	    frame.getContentPane().add(send, BorderLayout.SOUTH);
+	      
+	    send.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					send.setText("Waiting...");
+					send.setEnabled(false);
+					sendto.setEnabled(false);
+					Path path;
+					File dir = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+					filepath = dir.toString();
+					path = dir.toPath();
+					ListenThread emailThread = new ListenThread(path, sendto.getText(), sendfrom.getText(), password.getPassword());
+					emailThread.start();
+				}
+			});
+	 
+	      //Display the window.
+	      frame.pack();
+	      frame.setVisible(true);
+	}
+
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable (){
+			public void run() {
+				window();
+			}
+		});
+	}
+}
